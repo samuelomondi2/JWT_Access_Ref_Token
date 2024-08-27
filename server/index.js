6d910400ffbe3fc2108dbbae40d288718e234a21
Samuel Omondi <omondisam696@gmail.com>
2024-08-26 23:27:15 -0600

    register user

--- server/index.js
+++ server/index.js
@@ -4,9 +4,23 @@ import mongoose from 'mongoose';
 import jwt from 'jsonwebtoken';
 import cookieParser from 'cookie-parser';
 
+// Model
+import UserModel from './models/User';
+
 const app = express();
 app.use(express.json());
 app.use(cors());
+
+// connect to database
+mongoose.connect('');
+
+// register user
+app.post('/register', (req, res) => {
+    const {name, email, password} = req.body;
+    UserModel.create({name, email, password})
+        .then(user => res.json(user))
+        .catch(err => res.json(err))
+})
 
 app.listen(3001, () => {
     console.log("Server is Running...");
