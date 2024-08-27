2c111924d13cf5526b2e231ed33878dc84ff3633
Samuel Omondi <omondisam696@gmail.com>
2024-08-26 23:53:00 -0600

    first test to db

--- server/index.js
+++ server/index.js
@@ -3,16 +3,20 @@ import cors from 'cors';
 import mongoose from 'mongoose';
 import jwt from 'jsonwebtoken';
 import cookieParser from 'cookie-parser';
+import dotenv from 'dotenv';
+dotenv.config();
 
 // Model
-import UserModel from './models/User';
+import UserModel from './models/User.js';
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
 // connect to database
-mongoose.connect('');
+mongoose.connect(process.env.MONGO_URL)
+    .then(() => console.log("Connected to the database"))
+    .catch(() => console.log("Could not connect to the database"))
 
 // register user
 app.post('/register', (req, res) => {
--- server/models/User.js
+++ server/models/User.js
@@ -6,5 +6,5 @@ const UserSchema = new mongoose.Schema({
     password: {type: String, required: true}
 });
 
-const UserModel = mongoose.model('users', UserSchema);
-module.exports = UserModel;
+const UserModel = mongoose.model('users', UserSchema)
+export default UserModel;
