743ce6342afdc6caac59f2b3e668c80616fa583b
Samuel Omondi <omondisam696@gmail.com>
2024-08-26 23:19:40 -0600

    registration model

--- server/index.js
+++ server/index.js
@@ -5,6 +5,8 @@ import jwt from 'jsonwebtoken';
 import cookieParser from 'cookie-parser';
 
 const app = express();
+app.use(express.json());
+app.use(cors());
 
 app.listen(3001, () => {
     console.log("Server is Running...");
