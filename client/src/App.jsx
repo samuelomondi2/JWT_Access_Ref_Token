8fa727e030138218875924dfcfe14c714cb8d7bc
Samuel Omondi <omondisam696@gmail.com>
2024-08-27 22:49:10 -0600

    adding routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,19 @@
 import './App.css'
+import Home from './Home';
+import Login from './Login';
 import Registration from './Registration';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
 
 function App() {
 
   return (
-    <>
-      <Registration/>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home/>}></Route>
+        <Route path='/register' element={<Registration/>}></Route>
+        <Route path='/login' element={<Login/>}></Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
